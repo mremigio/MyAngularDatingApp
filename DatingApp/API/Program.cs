0af25b60a6b043605ef58290e0defb86fb303f0c
Mila <Mila.Remigio@research.ucla.edu>
2024-03-01 15:07:42 -0800

    Add CORS policy

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -29,6 +30,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
