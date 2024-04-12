59d11f670363fa101864be46a48464c4d5089124
Mila <Mila.Remigio@research.ucla.edu>
2024-04-12 15:06:44 -0700

    Add bootstrap and font awesome

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -8,13 +8,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-  title = 'Hi Georgie!!!';
+  title = 'Hi Georgie Porgie!!!';
   users: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
+    this.http.get('https://localhost:44386/api/Users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
