262b27bcb65049e6eb6eb510a7a4c85c4f0a3567
Mila <Mila.Remigio@research.ucla.edu>
2024-05-03 15:56:48 -0700

    Add Base Controller

---
+++ DatingApp/API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    public BaseApiController()
+    {
+    }
+}
