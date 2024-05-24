26fcb488fe4da34acdf9f8a890a128690c05be53
Mila <Mila.Remigio@research.ucla.edu>
2024-05-23 18:30:34 -0700

    Save Account controller

---
+++ DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] //POST: api/account/register
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+    }
+}
