086e8afe37c1d31087f9967c4a9008065bba4b2d
Mila.Remigio@research.ucla.edu <Mila.Remigio@research.ucla.edu>
2024-07-01 16:22:59 -0700

    Add FormsModule

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  exportAs: 'ngForm'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn = false;
 
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    console.log('Nav Init');
+  }
+
+  login() {
+    console.log('Nav Login');
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
 }
