7934e9629087cc7a77fc2d0cd41e4830a0d10309
Mila.Remigio@research.ucla.edu <Mila.Remigio@research.ucla.edu>
2024-07-18 21:21:32 -0700

    Add guard to member list

---
+++ DatingApp/client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+  return accountService.currentUser$.pipe(
+    map(user => {
+      if (user) return false;
+      else {
+        toastr.error('you shall not pass ');
+        return false;
+      }
+    })
+  )
+};
