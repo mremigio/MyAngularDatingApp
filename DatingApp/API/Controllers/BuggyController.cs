39f94852997ebeac3f9bea46debff6d6d709486c
Mila.Remigio@research.ucla.edu <Mila.Remigio@research.ucla.edu>
2024-08-03 19:53:45 -0700

    Add Error handling (buggy) controller

---
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -0,0 +1,63 @@
+﻿using API.Data;
+using API.Entities;
+using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using API.Services;
+using API.Interfaces;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private DataContext _context;
+        private ITokenService _tokenService;
+        public BuggyController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if (thing == null) 
+            {
+                return NotFound();
+            }
+
+            return thing;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+
+
+    }
+}
--- DatingApp/API/DTOs/RegisterDto.cs
+++ DatingApp/API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         public string Username { get; set; } = string.Empty;
 
         [Required]
+        [StringLength(8, MinimumLength =4)]
         public string Password { get; set; } = string.Empty;
     }
 }
