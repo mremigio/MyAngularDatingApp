1198f1c871a8743e00ac88b3cc2317a1817c46b6
Mila.Remigio@research.ucla.edu <Mila.Remigio@research.ucla.edu>
2024-08-13 13:14:02 -0700

    Enhance not found routing

--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -16,8 +16,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
-import { NotFoundComponent } from './error/not-found/not-found.component';
-import { ServerErrorComponent } from './error/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 @NgModule({
